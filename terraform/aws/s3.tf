e6dc05aea9158dab3f92045df8b7761f6787ee2f
shaharsa <50098571+shaharsa@users.noreply.github.com>
2021-11-03 16:15:14 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "data" {
   })
 }
 
-resource "aws_s3_bucket_object" "data_object123wwsadsasdasddasd" {
+resource "aws_s3_bucket_object" "data_object123wwdsadsasdasddasd" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
